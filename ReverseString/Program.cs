b2a601ab1b51dc589e4da43c59b095736a19f0d0
Paulino <paulino.benavides@fiserv.com>
2019-06-28 17:45:19 -0600

    Full-Program

---
+++ ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write your word/sentence you want to reverse: ");
+            var word = Console.ReadLine();
+			var result = ReverseString.Reverse(word);
+			Console.WriteLine(string.Format("You write: {0} and the reverse is: {1}", word, result));
+			Console.ReadKey();
+        }
+    }
+
+ public static class ReverseString
+ {
+     public static string Reverse(string input) => string.Join(string.Empty, input.Reverse());
+ }
+}
+
+
